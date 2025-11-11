ec92015df792cbe6be942151419b969661d9e346
Massiel <mpperozo.22@est.ucab.edu.ve>
2025-11-11 19:28:46 -0400

    Se agrego las dependencias necesarias en el main.dart y se agrego codigo para estrablcer las conexiones con las implementaciones de las entidades. Adicional a eso se establecieron los colores dev la app, se agrego codigo para manejar fallos de red o algun fallo en una validacion y se agrego codigo para validar ciertos campos

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//Colores de la app, el fin es que esto san colores divertidos y que insentiven jugar
+class AppColor{
+  static const Color primary = Colors.red;
+  static const Color secundary = Colors.blue;
+  static const Color accent = Colors.yellow;
+  static const Color success = Colors.green;
+  static const Color error = Colors.redAccent;
+  static const Color background = Colors.white;
+  static const Color card = Colors.purple;
+}
