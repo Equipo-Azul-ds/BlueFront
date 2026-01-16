eae0db8d344d7ec439377d34be4a957dfea2cb96
highwir3 <hollowsouls2001@gmail.com>
2026-01-16 15:46:39 -0400

    ultimo cambio

--- lib/config/use_firebase.dart
+++ lib/config/use_firebase.dart
@@ -1,4 +1,4 @@
 /// Compile-time switch to enable/disable Firebase features.
 ///
 /// Esto es solo para poder compilar la aplicacion en chrome-web
-const bool kUseFirebase = bool.fromEnvironment('USE_FIREBASE', defaultValue: false);
+const bool kUseFirebase = bool.fromEnvironment('USE_FIREBASE', defaultValue: true);
